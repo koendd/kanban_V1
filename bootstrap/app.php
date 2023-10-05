03cb0e5bb56a559998bc9b6a28591dafab3d338a
Koen De Deckers <dedeckers.koen@gmail.com>
2023-10-05 20:25:45 +0200

    add owner html content

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,5 +51,8 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-
+print('<!--
+This applicantion is created and onwed by Koen De Deckers.
+It is intended for personal use and not as a commercial application.
+-->');
 return $app;
